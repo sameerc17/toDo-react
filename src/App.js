24779c817d0f1584e1a1de8981bb54a33dec2745
Sameer Cherwoo <sameer1771999@gmail.com>
2022-07-26 21:21:58 +0530

    edit and delete functionality

--- src/App.js
+++ src/App.js
@@ -4,19 +4,45 @@ import List from './List';
 function App() {
 
   const [text, setText] = useState('')
+  const [editID, setEditID] = useState(null)
   const [items, setItems] = useState([])
+  const [editingAlert, setEditingAlert] = useState(false)
 
   const onHandleSubmit = (e) => {
     e.preventDefault();
-    if (text) {
-      const item = { id: new Date().getTime().toString(), title: text }
-      setItems([...items, item])
+    if (text && editingAlert) {
+      setItems(items.map((item) => {
+        if (item.id === editID) {
+          return { ...item, title: text };
+        }
+        return item;
+      })
+      );
       setText('')
+      setEditingAlert(false)
+      setEditID(null)
     }
+    else
+      if (text) {
+        const item = { id: new Date().getTime().toString(), title: text }
+        setItems([...items, item])
+        setText('')
+      }
   }
 
   const clearAllItems = () => {
     setItems([])
+  }
+
+  const clearItem = (id) => {
+    setItems(items.filter((item) => item.id !== id));
+  }
+
+  const editItem = (id) => {
+    const specificItem = items.find((item) => item.id === id);
+    setEditingAlert(true)
+    setEditID(id)
+    setText(specificItem.title)
   }
 
   return <>
@@ -34,12 +60,12 @@ function App() {
           <button
             type='submit'
             className='btn'
-          >Add item</button>
+          > {editingAlert ? 'Edit item' : 'Add item'}</button>
         </form>
         <br></br>
       </div>
       {items.length > 0 && <div>
-        <List className='list' items={items} />
+        <List className='list' items={items} clearItem={clearItem} editItem={editItem} />
         <button onClick={clearAllItems}>Clear All Items</button>
       </div>}
     </section>
--- src/List.js
+++ src/List.js
@@ -1,9 +1,15 @@
-const List = ({ items }) => {
+import './App.css';
+const List = ({ items, clearItem, editItem }) => {
     return <div>
         {items.map((item) => {
             const { id, title } = item;
             return <article className='list_item'>
-                <p key={id}>{title}</p>
+                <div className="item">
+                    <p key={id}>{title}</p>
+                    <button className='edit_btn' onClick={() => editItem(id)}>Edit</button>
+                    <button className='del_btn' onClick={() => clearItem(id)}>Delete</button>
+                </div>
+
             </article>
         })}
     </div>
